09e9a28885205681018975faa6f58a39c1eae72d
Matt <matthew.rebehn@gmail.com>
2020-08-11 18:38:08 -0400

    added breakpoints & theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,19 @@
-import { ThemeProvider, CSSReset } from '@chakra-ui/core'
+import { theme, ThemeProvider, CSSReset } from '@chakra-ui/core'
+
+const breakpoints = ['360px', '768px', '1024px', '1440px']
+breakpoints.sm = breakpoints[0]
+breakpoints.md = breakpoints[1]
+breakpoints.lg = breakpoints[2]
+breakpoints.xl = breakpoints[3]
+
+const newTheme = {
+  ...theme,
+  breakpoints,
+}
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider>
+    <ThemeProvider theme={newTheme}>
       <CSSReset />
       <Component {...pageProps} />
     </ThemeProvider>
